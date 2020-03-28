34d928fe51b340bc26255c83ec7f29b0685f1b17
arosenthal <aaron.rosenthal@disneystreaming.com>
2020-03-28 15:26:46 -0400

    Add get lambda code

--- src/GetDiscounts/index.js
+++ src/GetDiscounts/index.js
@@ -1,3 +1,22 @@
-exports.handler = async (event, context, callback) => {
+const AWS = require('aws-sdk');
+const dynamo = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async event => {
     console.log(JSON.stringify(event, null, 2));
+    const params = {
+        TableName: "discounts"
+    };
+    try {
+        const data = await dynamo.scan(params).promise();
+        const response = {
+            statusCode: 200,
+            body: JSON.stringify(data.Items)
+        }
+        return response;
+    } catch(e) {
+        console.log("Error getting items from dynamo: " + e);
+        return {
+            statusCode: 500
+        }
+    }
 };
